151e32feb96ddaca0052a0fa3594163c16586730
biffysix <mrbrucewong@gmail.com>
2021-03-15 22:52:58 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,58 @@
+//require('dotenv').config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session"); //passport sits on express-sessions
+const path = require("path");
+const port = process.env.port || 8000;
+
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+const passport = require("./middleware/passport");
+//above imports from middleware/passports.js folder ie exports
+const authRoute = require("./routes/authRoute");
+const indexRoute = require("./routes/indexRoute");
+
+// Middleware for express
+app.use(express.json());
+app.use(expressLayouts);
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());//calls passport with new strat
+app.use(passport.session());  //calls passport with new strat
+
+app.use((req, res, next) => {
+  console.log(`User details are: `);
+  console.log(req.user);
+
+  console.log("Entire session object:");
+  console.log(req.session);
+
+  console.log(`Session details are: `);
+  console.log(req.session.passport);
+  next();
+});
+
+app.use("/", indexRoute);
+app.use("/auth", authRoute);//login page request comes here
+//then goes to authRoute file > router.post and get triggered
+
+//console.log(require('dotenv').config());
+
+app.listen(port, () => {
+  console.log(`🚀 Server has started on port ${port}`);
+});
